7f47cc7e5e20fec09eb7d89d80e0f78c2d1449ab
unknown <02230099.cst@rub.edu.bt>
2024-04-12 23:41:21 -0800

    assignment 1

--- CAP1_02230099.py
+++ CAP1_02230099.py
@@ -9,68 +9,47 @@
 #https://chat.openai.com/
 ################################
 # SOLUTION
-# Your Solution Score:
-# Put your number here
-################################
+# Solution Score:50267
 
-# This is a Function defined to read the input data from a file
+# Function is defined to read the input data from file
 def read_input(game_file):
-    # g_rounds variable initializes where it is a list to store game rounds data
-    g_rounds = []
-    
-    # This code opens the file for reading and is named as g_file
+    # Open the game file in read mode
     with open(game_file, 'r') as g_file:
-         # In order to read each line of the file, for loop is used to iterlate through. 
-        for line in g_file:
-             # It splits the line into individual elements
-            round_data = line.strip().split()
-            # Then it is appended as the tuple having player choice and outcome in the list g_rounds.
-            g_rounds.append((round_data[0], round_data[1]))
-    # Return the list of game rounds data
+        # Read each line from the file, stripping whitespace and splitting by space to get shape and result pairs.
+        g_rounds = [line.strip().split() for line in g_file] 
     return g_rounds
-
-# And function 'calculate_score' is defined to calculate the total score based on game rounds data.
-def calculate_score(g_rounds):
-    # Nested function to calculate score for each round
-    def calculate_round(player_choice, rival_choice, result):
-        # Dictionary options is declared to map player choices to scores
-        options = {'A': 1, 'B': 2, 'C': 3}
-        # Dictionary results to map game outcomes to scores
-        results = {'X': 0, 'Y': 3, 'Z': 6}
-        
-        # This block of codes below calculate scores for player, rival, and result
-        player_score = options[player_choice]
-        rival_score = options[rival_choice]
-        result_score = results[result]
-        
-        #This block of codes determine the result of the round(Draw, Win or Lose) and return the score
-        if player_score == rival_score: 
-            return player_score + result_score  # Draw
-        elif (player_score == 1 and rival_score == 3) or (player_score > rival_score):
-            return player_score + result_score  # Win
-        else:
-            return player_score + result_score  # Lose
-    
-    # Initialize total score to 0
-    total_score = 0
  
-    # Iteration through each game round
-    for round_data in rounds:
-        # Extract player choice and outcome
-        player_choice, outcome = round_data
-        # Determines the rival's choice based on player's choice
-        opponent_choice = 'C' if player_choice == 'A' else ('A' if player_choice == 'B' else 'B')
-        # Calculate score for the round and add to total score
-        total_score += calculate_round(player_choice, opponent_choice, outcome)
-    
-    # Return the total score
-    return total_score
+# Function "calculate_score" is defined to calculate final score based on game rounds data
+def calculate_score(g_rounds):
+    f_score = 0  # Setting the initial final score to zero.
+    # Calculating the score based on the combinations of shapes choice and players result
+    for shape_choice, players_result in g_rounds:  # Looping through each shape choice and result pair
+        if shape_choice == 'A' and players_result== 'X':
+            f_score += 3 # Adding 3 to the final score if the shape choice  is A and the result is X
+        elif shape_choice  == 'A' and players_result == 'Y':
+            f_score += 4  # Adding 4 to the final score if the shape choice is A and the result is Y
+        elif shape_choice == 'A' and players_result == 'Z':
+            f_score += 8  # Adding 8 to the final score if the shape choice is A and the result is Z
+        elif shape_choice == 'B' and players_result == 'X':
+            f_score += 1 # Adding 1 to the final score if the shape choice is B and the result is X
+        elif shape_choice == 'B' and players_result == 'Y':
+            f_score+= 5 # Adding 5 to the final score if the shape choice is B and the result is Y
+        elif shape_choice == 'B' and players_result == 'Z':
+            f_score += 9  # Adding 9 to the final score if the shape choice is B and the result is Z
+        elif shape_choice == 'C' and players_result == 'X':
+            f_score += 2  #  Adding 2 to the finsl score if the shape choice is C and the result is X
+        elif shape_choice== 'C' and players_result == 'Y':
+            f_score += 6  # Adding 6 to the final score if the shape choice is C and the result is Y
+        elif shape_choice == 'C' and players_result == 'Z':
+            f_score += 7 # Adding 7 to the final score if the shape choice is C and the result is Z
+    return f_score  # Returning the final total score
 
-# File name containing game rounds data is provided 
-file_name = "input_9_cap1.txt"
-# read input function is called and stored its data in rounds variable.
-rounds = read_input(file_name)
-# calculate_score is called and its values is stored in total score variable.
-total_score = calculate_score(rounds)
-# Print the total score
-print("Total Score:", total_score)
+
+file_p = "input_9_cap1.txt" # Defining the path to the input file
+
+
+# Read the game data from the file
+All_rounds = read_input(file_p)
+
+f_score = calculate_score(All_rounds)  # Compute the final score using the game data
+print("Total Score:", f_score)  # Print the total score
